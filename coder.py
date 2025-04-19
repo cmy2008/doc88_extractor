31f9c5954d8efe6221573b468c93d3296b2cdd03
Cuite Piglin <65344456+cmy2008@users.noreply.github.com>
2025-04-20 01:28:25 +0800

    fix typo

--- coder.py
+++ coder.py
@@ -5,7 +5,7 @@ key2 = "PJKLMNOI3xyz012wvprqstuoHBCDEFGAnhijklmgfZabcdeYXRSTUVWQ!56789+4"
 std_str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 
 
-def encode(data: str, key: list = key1) -> str:
+def encode(data: str, key: str = key1) -> str:
     return (
         base64.b64encode(data.encode("utf-8"))
         .decode("utf-8")
@@ -13,5 +13,5 @@ def encode(data: str, key: list = key1) -> str:
     )
 
 
-def decode(data: str, key: list = key1) -> str:
+def decode(data: str, key: str = key1) -> str:
     return base64.b64decode(data.translate(str.maketrans(key, std_str))).decode("utf-8")
