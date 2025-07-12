a523f6a492b0921f501ecdf86a4d204362138ba0
Cuite Piglin <65344456+cmy2008@users.noreply.github.com>
2025-07-12 17:22:43 +0800

    优化compressor.py

--- compressor.py
+++ compressor.py
@@ -2,31 +2,17 @@ import struct
 import zlib
 
 class Compressor:
-    MAGIC_ZWS = b"ZWS"
-    MAGIC_CWS = b"CWS"
-    MAGIC_FWS = b"FWS"
-    MAGIC_EBT = b"YBD"
-    MAGIC_EBK = b"EBT_PK"
-
     def __init__(self):
-        self.ref = None
-        self.buffer = None
-        self.glue = b""
-        self.dos = b"Nothing"
-        self.MAKEUP = True
+        return None
 
     def load(self, file):
         with open(file, 'rb') as f:
-            self.ref = f.read()
-        return self.ref
+            return f.read()
 
     def processSWF(self,file_EBT,file_EBK,path):
-        swf = None
-        swf = self.decompressEBT_PH(self.load(file_EBT))
-        swf = self.decompressEBT_PK(self.load(file_EBK))
-        is_ebt = self.glue == self.MAGIC_EBT
-        swf = self.makeup(self.buffer if is_ebt else swf, swf if is_ebt else self.buffer)
-        self.glue = None
+        ph = self.decompressEBT_PH(self.load(file_EBT))
+        pk = self.decompressEBT_PK(self.load(file_EBK))
+        swf = self.makeup(ph,pk)
         with open(path, 'wb') as f:
             f.write(swf)
 
@@ -39,60 +25,19 @@ class Compressor:
         return buff
 
     def decompressEBT_PH(self, data):
-        data = data[40:]
         buff = bytearray()
-        ebt = bytearray(data)
-
         try:
-            decompressed = zlib.decompress(ebt)
-            buff.extend(decompressed)
+            buff.extend(zlib.decompress(data[40:]))
             buff[4:8] = struct.pack('<I', len(buff))
         except zlib.error:
             return None
-
-        if not self.buffer:
-            self.buffer = buff
-            self.glue = self.MAGIC_EBT
-
         return buff
 
     def decompressEBT_PK(self, data):
-        data = data[32:]
-        ebt = bytearray()
-        buff = bytearray(data)
-
         try:
-            decompressed = zlib.decompress(buff)
-            ebt.extend(decompressed)
+            return zlib.decompress(data[32:])
         except zlib.error:
             return None
-
-        if not self.buffer:
-            self.buffer = buff
-            self.glue = self.MAGIC_EBK
-
-        return ebt
-
-    def compress(self, data):
-        header = data[3:8]
-        decompressed = data[8:]
-        compressed = bytearray()
-
-        compressed.extend(b"CWS")
-        compressed.extend(header)
-        compressed.extend(zlib.compress(decompressed))
-
-        return compressed
-
-    def decompress(self, data):
-        header = data[3:8]
-        compressed = data[8:]
-        decompressed = bytearray()
-
-        decompressed.extend(header)
-        decompressed.extend(zlib.decompress(compressed))
-
-        return decompressed
 
 def make(file_EBT,file_EBK,path):
     compressor = Compressor()
