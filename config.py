811f68a0c7215f5819f0667592f9514eb498c876
cmy2008 <65344456+cmy2008@users.noreply.github.com>
2026-09-12 20:11:45 +0800

    fix: 撤回ffdec更新仓库配置

--- config.py
+++ config.py
@@ -18,13 +18,13 @@ class Config:
     def __init__(self, config_path: str = "config.json") -> None:
         self.default_config: dict[str, Any] = {
             "version": "2.3",
-            "ffdec_version": "version26.2.1",
+            "ffdec_version": "version26.2.2",
             "o_dir_path": "docs/",
             "o_swf_path": "swf/",
             "o_pdf_path": "pdf/",
             "o_svg_path": "svg/",
             "proxy_url": "",
-            "ffdec_repo": "jindrapetrik/jpexs-decompiler",
+            "ffdec_repo": "cmy2008/jpexs-decompiler",
             "svg2pdf_repo": "cmy2008/svg2pdf",
             "presse_repo" : "cmy2008/presse",
             "check_update": True,
